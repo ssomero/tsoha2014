6fde79dea2cd4a33a66db45395e94b8ab0a69e70
ssomero <sonja.somero@helsinki.fi>
2014-03-18 20:49:46 +0200

    demosivua jatkettu

--- drinkit.php
+++ drinkit.php
@@ -8,13 +8,61 @@
         <link href="css/main.css" rel="stylesheet">
     </head>
     <body>
-        <ul class="nav nav-tabs">
-            <li><a href="html-demo.html">Etusivu</a></li>
-            <li><a href="haku.php">Haku</a></li>
-            <li class="active"><a href="drinkit.php">Drinkit</a></li>
-        </ul>
+        <div class="container">
+            <form class="navbar-form navbar-right" role="form">
+                <div class="form-group">
+                    <input type="text" placeholder="Käyttäjätunnus" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="Salasana" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-success">Kirjaudu sisään!</button>
+            </form>
+            <ul class="nav nav-tabs">
+                <li><a href="html-demo.html">Etusivu</a></li>
+                <li><a href="haku.php">Haku</a></li>
+                <li class="active"><a href="drinkit.php">Drinkit</a></li>
+            </ul>
 
-        <h1>Drinkit</h1>
-        <p>Tässä lista Drinkkiarkistosta löytyvistä resepteistä</p>
+            <h1>Drinkit</h1>
+            <p>Tässä lista Drinkkiarkistosta löytyvistä resepteistä</p>
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nimi</th>
+                        <th>Pääainesosa</th>
+                        <th>Juomalaji</th>
+                        <th>Lisääjä</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mojito</td>
+                        <td>Vaalea rommi</td>
+                        <td>cocktail</td>
+                        <td>ylläpitäjä</td>
+                    </tr>
+                    <tr>
+                        <td>Piña Colada</td>
+                        <td>Vaalea rommi</td>
+                        <td>cocktail</td>
+                        <td>ylläpitäjä</td>
+                    </tr>
+                    <tr>
+                        <td>Sitrusbooli</td>
+                        <td>Vodka</td>
+                        <td>booli</td>
+                        <td>käyt123</td>
+                    </tr>
+                    <tr>
+                        <td>ÄssäMix</td>
+                        <td>Useita</td>
+                        <td>shotti</td>
+                        <td>ylläpitäjä</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
--- haku.php
+++ haku.php
@@ -1,4 +1,4 @@
-<?php  ?>
+<?php ?>
 
 <html>
     <head>
@@ -9,13 +9,30 @@
         <link href="css/main.css" rel="stylesheet">
     </head>
     <body>
-        <ul class="nav nav-tabs">
-            <li><a href="html-demo.html">Etusivu</a></li>
-            <li class="active"><a href="haku.php">Haku</a></li>
-            <li><a href="drinkit.php">Drinkit</a></li>
-        </ul>
+        <div class="container">
+            <form class="navbar-form navbar-right" role="form">
+                <div class="form-group">
+                    <input type="text" placeholder="Käyttäjätunnus" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="Salasana" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-success">Kirjaudu sisään!</button>
+            </form>
+            <ul class="nav nav-tabs">
+                <li><a href="html-demo.html">Etusivu</a></li>
+                <li class="active"><a href="haku.php">Haku</a></li>
+                <li><a href="drinkit.php">Drinkit</a></li>
+            </ul>
 
-        <h1>Drinkkireseptien hakeminen</h1>
-        <p>Anna hakusanaksi esim. drinkin nimi tai jokin ainesosa</p>
+            <h1>Drinkkireseptien hakeminen</h1>
+            <p>Anna hakusanaksi esim. drinkin nimi tai jokin ainesosa</p>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="nimi, ainesosa,...">
+                </div>
+                <button type="submit" class="btn btn-primary">Hae</button>
+            </form>
+        </div>
     </body>
 </html>
