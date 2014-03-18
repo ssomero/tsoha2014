09fbde176ffd981b60ba396ac00de7c11893836b
ssomero <sonja.somero@helsinki.fi>
2014-03-18 19:43:08 +0200

    html-demo-sivun alku

---
+++ drinkit.php
@@ -0,0 +1,20 @@
+<?php ?>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <ul class="nav nav-tabs">
+            <li><a href="html-demo.html">Etusivu</a></li>
+            <li><a href="haku.php">Haku</a></li>
+            <li class="active"><a href="drinkit.php">Drinkit</a></li>
+        </ul>
+
+        <h1>Drinkit</h1>
+        <p>Tässä lista Drinkkiarkistosta löytyvistä resepteistä</p>
+    </body>
+</html>
---
+++ haku.php
@@ -0,0 +1,21 @@
+<?php  ?>
+
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <ul class="nav nav-tabs">
+            <li><a href="html-demo.html">Etusivu</a></li>
+            <li class="active"><a href="haku.php">Haku</a></li>
+            <li><a href="drinkit.php">Drinkit</a></li>
+        </ul>
+
+        <h1>Drinkkireseptien hakeminen</h1>
+        <p>Anna hakusanaksi esim. drinkin nimi tai jokin ainesosa</p>
+    </body>
+</html>
